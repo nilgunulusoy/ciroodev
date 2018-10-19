30b4e1db08308cf6713eccf36e24d0d6e14abff2
Ecem Er <072416@tt-tim.tr>
2018-10-19 19:23:07 +0300

    initial commit

---
+++ deneme2.py
@@ -0,0 +1,13 @@
+ogrNum=input()
+
+queryNum=int(ogrNum[0:4])
+if(queryNum%4==0):
+    print("oys1")
+elif (queryNum%4==1):
+    print("oys2")
+elif (queryNum%4==2):
+    print("oys3")
+else:
+    print("oys4")
+
+print (2*3)
